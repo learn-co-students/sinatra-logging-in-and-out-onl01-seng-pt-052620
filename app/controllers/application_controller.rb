223c748eaa6dac9e2897aa26a539d85db22b59f1
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-08-19 05:09:04 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    #binding.pry
+    if @user = User.find_by(username: params[:username])
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      erb :error
+    end
   end
 
   get '/account' do
+    #binding.pry
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
   get '/logout' do
+
+    session.clear
+    redirect "/"
 
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,19 @@
 class Helpers
+
+    def self.current_user(session)
+        #binding.pry
+        User.find_by(id: session[:user_id])
+
+    end
+
+    def self.is_logged_in?(session)
+    
+        if session[:user_id].nil?
+            false
+        else
+            !!User.find(session[:user_id])
+        end
+
+    end
+
 end
