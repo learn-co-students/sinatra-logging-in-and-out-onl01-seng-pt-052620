da82dd4b5c62a643fe9e30934e641b77b9b2ae0c
M. Gage Morgan <gage@gages.blog>
2020-08-26 08:55:33 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    redirect "/error"
   end
 
   get '/account' do
